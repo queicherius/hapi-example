b219526c52005d1d375f8a8e31e3159b847e3e64
queicherius <queicherius@gmail.com>
2017-02-27 02:43:15 +0100

    Real world container amount

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Building images..."
 docker-compose build
+
+echo "Tearing down old stack..."
 docker stack rm gw2efficiency
-sleep 5
+
+while [[ $(docker network ls | grep 'gw2efficiency_default') != "" ]]
+do
+  echo "Waiting for network teardown..."
+  sleep 1
+done
+
+sleep 3
+echo "Deploying stack..."
 docker stack deploy --compose-file docker-compose.yml gw2efficiency
+
+while true
+do
+  sleep 1
+  cls
+  docker stack services gw2efficiency
+done
--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,3 @@
 var uuid = require('uuid')
 
-console.log('Hello there fellow kids.<br>How do you do?<br><br><small>' + uuid() + '</small>')
+console.log('How do you do, fellow kids?<br><small>' + uuid() + '</small>')
